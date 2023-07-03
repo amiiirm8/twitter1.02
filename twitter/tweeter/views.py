3e390a551a91c41360f35c18b6d0b8a2c3b759a2
narges <119301788+amiiirm8@users.noreply.github.com>
2023-07-03 13:42:44 +0330

    feat:adding login and logout

--- twitter/tweeter/views.py
+++ twitter/tweeter/views.py
@@ -3,6 +3,10 @@ from .models import Profile
 from django.contrib import messages
 from .forms import TweetForm
 from .models import Tweet
+from django.contrib.auth import authenticate, login,logout
+
+
+
 
 def home(request):
     if request.user.is_authenticated:
@@ -49,3 +53,24 @@ def profile(request, pk):
         messages.success(request, ("You must be logged in to view this page... "))
         return redirect('home')
     
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You Have Been Logged In!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error Logging in. Please Try Again"))
+            return redirect('login')
+
+    else:
+        return render(request, "login.html", {}) 
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You Have Been Logged Out."))
+    return redirect('home')
